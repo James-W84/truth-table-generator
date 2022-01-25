b5c60233f0f8c6fc5a819722a8e1432719a0a5dd
James Wu <jameswu@Jamess-MacBook-Air-2.local>
2022-01-25 14:31:10 -0600

    change import location react

--- src/Output.js
+++ src/Output.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react";
 import ButtonGroup from "./ButtonGroup";
 
 function Output({ outputNum, formInputs, handleChange, addOperator }) {
